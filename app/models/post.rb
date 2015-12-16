75ece66e11a68a09d43ebcbf8d9367d11710cbbe
Chris Mangano <chris.mangano@me.com>
2015-12-15 19:52:48 -0500

    Added association between user and post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
 	validates :body, presence: true, length: { minimum: 5 }
 	has_attached_file :photo, styles: { medium: "600x450>" }
 	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+  belongs_to :user
 end
