2f34b0831398d1bb0c4d1b0d018eced7b06ae626
Chris Mangano <chris.mangano@me.com>
2015-12-21 21:28:20 -0500

    Sendgrid bug fix.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://destroyboredom.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
   # Sets paperclip to Upload to Amazon S3 by default
   config.paperclip_defaults = {
     :storage => :s3,
